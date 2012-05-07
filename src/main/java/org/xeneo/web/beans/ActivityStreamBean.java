d355b5398cce87bf57b8a7bb3389603c03abd7da
Stefan Huber <stefan.huber.mail@gmail.com>
2012-05-07 19:00:03 +0200

    first commit

---
+++ src/main/java/org/xeneo/web/beans/ActivityStreamBean.java
@@ -0,0 +1,26 @@
+
+package org.xeneo.web.beans;
+
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Scope;
+import org.springframework.stereotype.Component;
+import org.xeneo.core.activity.Activity;
+import org.xeneo.core.activity.ActivityStream;
+
+/**
+ *
+ * @author Stefan Huber
+ */
+
+@Component("stream")
+@Scope("request")
+public class ActivityStreamBean {
+    
+    @Autowired
+    private ActivityStream as;  
+    
+    public List<Activity> getActivityStream() {
+        return as.getActivities("http://blub", "http://blab", 50);
+    }    
+}
